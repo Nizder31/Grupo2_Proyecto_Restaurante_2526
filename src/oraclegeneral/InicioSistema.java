82d0e1fd33a7051ee3c376c92f2f332f8f6b562b
Nizder31 <noahalbacasado@gmail.com>
2025-12-03 22:12:58 +0100

    Modificacion InicioSistema A1

--- src/oraclegeneral/InicioSistema.java
+++ src/oraclegeneral/InicioSistema.java
@@ -7,6 +7,7 @@ import models.Producto;
 import models.Usuario;
 import views.Login;
 /**
+ * --Cambio generado por A1--
  * Clase que iniciara el sistema y verificara la existencia del usuario de la base de datos que se 
  * utilizara a lo largo de la ejecucion.
  * @author Erik David Zubia Hernandez.
